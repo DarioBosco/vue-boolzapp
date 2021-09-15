96e33ca4139a174a317275c5d1a92ce2d0f581c3
DarioBosco <dario_bosco@protonmail.com>
2021-09-15 13:59:01 +0200

    Milestone 4

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,14 @@
 *Milestone 3
 ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando“enter” il testo viene aggiunto al thread sopra, come messaggio verde
 ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+*Milestone 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+Milestone 5 - BONUS
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
+
 */
 
 Vue.config.devtools = true;
@@ -18,6 +26,7 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		selectedUser: 0,
+		userLookupText: '',
 		userMessage: '',
 		user: {
 			name: 'User Name',
@@ -132,6 +141,11 @@ const app = new Vue({
 					status: 'received',
 				});
 			}, 1000);
+		},
+		userLookup() {
+			return this.contacts.filter((contact) => {
+				return contact.name.toLowerCase().includes(this.userLookupText.toLowerCase());
+			});
 		},
 	},
 });
