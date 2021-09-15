ba3235bfe5753bef6274bdf5525af33f2fd4460d
DarioBosco <dario_bosco@protonmail.com>
2021-09-15 12:32:52 +0200

    Milestone 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 /* 
-Milestone 1:
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
-dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
-nome e immagine di ogni contatto
+*Milestone 1:
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
-Milestone 2:
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato
+*Milestone 2:
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+● Click sul contatto mostra la conversazione del contatto cliccato
+
+*Milestone 3
+● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando“enter” il testo viene aggiunto al thread sopra, come messaggio verde
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
 Vue.config.devtools = true;
@@ -16,6 +18,7 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		selectedUser: 0,
+		userMessage: '',
 		user: {
 			name: 'User Name',
 			avatar: '_io',
@@ -109,6 +112,26 @@ const app = new Vue({
 	methods: {
 		getSelectedUser(index) {
 			this.selectedUser = index;
+		},
+		sendMessage() {
+			/* Invio del messaggio dell'utente */
+			let currentUser = this.selectedUser;
+			let tmpArray = this.contacts[currentUser].messages;
+			tmpArray.push({
+				date: dayjs().format('DD/MM/YYYY hh:mm:ss'),
+				message: this.userMessage,
+				status: 'sent',
+			});
+			this.userMessage = '';
+
+			/* Invio del messaggio di risposta */
+			setTimeout(function () {
+				tmpArray.push({
+					date: dayjs().format('DD/MM/YYYY hh:mm:ss'),
+					message: 'Ok',
+					status: 'received',
+				});
+			}, 1000);
 		},
 	},
 });
